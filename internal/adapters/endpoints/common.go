694e4a4be64e2d8829d680d736c87a6b95dfad1d
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2020-08-02 20:26:37 +0700

    refactor: insert error in the response object

--- internal/adapters/endpoints/common.go
+++ internal/adapters/endpoints/common.go
@@ -3,10 +3,19 @@ package endpoints
 import (
 	"context"
 	"encoding/json"
+	kithttp "github.com/go-kit/kit/transport/http"
 	"net/http"
 )
 
-func DefaultRequestEncoder(_ context.Context, w http.ResponseWriter, response interface{}) error {
+type errorer interface {
+	Error() error
+}
+
+func DefaultRequestEncoder(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	if err, ok := response.(errorer); ok && err != nil {
+		encodeError(ctx, err.Error(), w)
+		return nil
+	}
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(response)
 }
@@ -28,5 +37,22 @@ func DefaultRequestDecoder(decoder func(r *http.Request) (UserClaimable, error))
 		}
 		request.SetUserClaim(*userClaim)
 		return request, nil
+	}
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	switch err {
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_ = json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
+}
+
+func GetDefaultHTTPOptions() []kithttp.ServerOption {
+	return []kithttp.ServerOption{
+		kithttp.ServerErrorEncoder(encodeError),
 	}
 }
